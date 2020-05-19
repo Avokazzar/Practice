6c01e70ba607d01fcde7a146ad493f16ee5671f5
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-05-19 22:51:36 +0300

    Add files via upload

---
+++ Ex_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double a, b, c, d, x1, x2;
+    std::cout << "Вход:\n";
+    std::cin >> a >> b >> c;
+    std::cout << "Выход:\n";
+    d = (pow(b, 2)) - (4 * a * c);
+    if (d > 0) {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        std::cout << x1 << " " << x2 << "\n";
+    }
+    else if (d == 0) {
+        x1 = -b / (2 * a);
+        std::cout << x1 << "\n";
+    }
+    else {
+        std::cout << "\n";
+    }
+}
