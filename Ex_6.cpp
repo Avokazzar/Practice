1d7f46f5e1bfc3b4df60f26717c0cdbb087502e7
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-05-22 01:48:03 +0300

    Add files via upload

---
+++ Ex_6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>    
+
+int IsPalindrom(std::string s)
+{
+    for (int i = 0; i <= s.length(); i++) {
+        if (s[i] == ' ') {
+            s.erase(i, 1);
+        }
+        s[i] = tolower(s[i]);
+    }
+    std::string s_new = s;
+    reverse(s_new.begin(), s_new.end());
+    bool res;
+    if (s == s_new) {
+        res = true;
+    }
+    else {
+        res = false;
+    }
+    return res;
+}
+
+int main()
+{
+    std::string s;
+    std::cout << "Input:\n";
+    getline(std::cin, s);
+    
+    std::cout << "Output:\n" << IsPalindrom(s);
+}
