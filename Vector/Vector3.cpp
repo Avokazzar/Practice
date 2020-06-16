847e633e0ece891f89ea288c24b426e56501c917
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-06-17 00:42:23 +0300

    Update Vector3.cpp

--- Vector/Vector3.cpp
+++ Vector/Vector3.cpp
@@ -39,26 +39,26 @@ Vector3& Vector3::operator=(const Vector3& other) {// +
 }
 
 Vector3& Vector3::operator+=(const Vector3& other) {
-    for (int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < n; i++)
         elem[i] += other[i];
     return *this;
 }
 
 Vector3& Vector3::operator-=(const Vector3& other) {
-    for (int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < n; i++)
         elem[i] -= other[i];
     return *this;
 }
 
 Vector3& Vector3::operator*=(const double number) {
-    for (int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < n; i++)
         elem[i] *= number;
     return *this;
 }
 
 Vector3& Vector3::operator/=(const double number) {
     double numOver = 1.0f / number;
-    for (int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < n; i++)
         elem[i] *= numOver;
     return *this;
 }
@@ -72,42 +72,34 @@ Vector3 operator+(const Vector3& our, const Vector3& other) {
 }
 
 Vector3 operator-(const Vector3& our, const Vector3& other) {
-    return { our.elem[0] - other.elem[0], our.elem[1] - other.elem[1], our.elem[2] - other.elem[2] };
+    return Vector3(our) -= other;
 }
 
 Vector3 operator*(const Vector3& our, double number) {
-    return our.elem[0] * number, our.elem[1] * number, our.elem[2] * number;
+    return Vector3(our) *= number;
 }
 
 Vector3 operator*(const double number, const Vector3& our) {
-    return number * our.elem[0], number* our.elem[1], number* our.elem[2];
+    return Vector3(our) *= number;
 }
 
 Vector3 operator/(const Vector3& our, const double number) {
     double numOver = 1.0f / number;
-    return our.elem[0] * numOver, our.elem[1] * numOver, our.elem[2] * numOver;
+    return Vector3(our) = numOver;
 }
 
 double Vector3::getNorma() const {
     return sqrt(elem[0] * elem[0] + elem[1] * elem[1] + elem[2] * elem[2]);
 }
 void Vector3::normalize() {
-    double mod = 0.0;
-    for (int i = 0; i < n; i++) {
-        mod += elem[i] * elem[i];
-    }
-    double mag = sqrt(mod);
+    double mag = sqrt(elem[0] * elem[0] + elem[1] * elem[1] + elem[2] * elem[2]);
     elem[0] /= mag;
     elem[1] /= mag;
     elem[2] /= mag;
 }
 Vector3 Vector3::getNormalized()const {
     Vector3 result(*this);
-    double mod = 0.0;
-    for (int i = 0; i < n; i++) {
-        mod += elem[i] * elem[i];
-    }
-    double mag = sqrt(mod);
+    double mag = sqrt(elem[0] * elem[0] + elem[1] * elem[1] + elem[2] * elem[2]);
     result.elem[0] /= mag;
     result.elem[1] /= mag;
     result.elem[2] /= mag;
