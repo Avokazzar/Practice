20e8d62b6c5cb6dc1bd3848e646dd33bb116bab0
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-06-16 23:47:16 +0300

    Update Files.cpp

--- Files.cpp
+++ Files.cpp
@@ -1,39 +1,33 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
+#include <algorithm> 
+#include <vector>
 #include <string>
 #include <stdlib.h>
 
 using std::cout;
 using std::cin;
 using std::endl;
-using std::stoi;
 using std::string;
-using std::ifstream;
-using std::ofstream;
-
-
+using std::min_element;
 
 int main()
 {
-    int num_1, num_2, ** array_row, ** array_col, temp;
+    int num_1, num_2, temp;
     double mean, sum = 0;
 
-    string line;
-    ifstream in("D:\\input.txt"); 
-    ofstream out_row("D:\\out_row.txt", std::ios::out);
-    ofstream out_col("D:\\out_col.txt", std::ios::out);
+    std::string line;
+    std::ifstream in("D:\\input.txt");
+    std::ofstream out_row("D:\\out_row.txt", std::ios::out);
+    std::ofstream out_col("D:\\out_col.txt", std::ios::out);
 
     if (in.is_open() && out_row.is_open())
     {
         in >> num_1 >> num_2;
 
-        array_row = new int* [num_1];
-        array_col = new int* [num_1];
-        for (int i = 0; i < num_1; i++) {
-            array_row[i] = new int[num_2];
-            array_col[i] = new int[num_2];
-        }
-        
+        std::vector<std::vector<int>> matrix_row(num_1, std::vector<int>(num_2));
+        std::vector<std::vector<int>> matrix_col(num_1, std::vector<int>(num_2));
+        std::vector<int> minmax_str;
         for (int i = 0; i < num_1; i++) {
             for (int k = 0; k < num_2; k++) {
                 if (k == num_2 - 1) {
@@ -42,68 +36,49 @@ int main()
                 else {
                     getline(in, line, ',');
                 }
-                array_row[i][k] = stoi(line);
-                array_col[i][k] = stoi(line);
+                matrix_row[i][k] = stoi(line);
+                matrix_col[i][k] = stoi(line);
+                sum += matrix_row[i][k];
+                minmax_str.push_back(matrix_row[i][k]);
             }
         }
+        auto minmax = std::minmax_element(minmax_str.begin(), minmax_str.end());
+        mean = sum / (num_1 * num_2);
+        
+        out_row << "mean = " << mean << endl;
+        out_col << "mean = " << mean << endl;
+        out_row << "max = " << *minmax.second << endl;
+        out_col << "max = " << *minmax.second << endl;
+        out_row << "min = " << *minmax.first << endl;
+        out_col << "min = " << *minmax.first << endl;
+ 
         for (int i = 0; i < num_1; i++) {
+            sort(matrix_row[i].begin(), matrix_row[i].end());
             for (int k = 0; k < num_2; k++) {
-                for (int j = num_2 - 1; j > k; j--) {
-                    if (array_row[i][j] < array_row[i][j - 1]) {
-                        temp = array_row[i][j];
-                        array_row[i][j] = array_row[i][j - 1];
-                        array_row[i][j - 1] = temp;
-                    }
-                }
+                out_row << matrix_row[i][k] << " ";
             }
+            out_row << endl;
         }
         for (int j = 0; j < num_2; j++) {
             for (int k = 0; k < num_1 - 1; k++) {
                 for (int i = 0; i < num_1 - k - 1; i++) {
-                    if (array_col[i][j] < array_col[i + 1][j]) {
-                        temp = array_col[i][j];
-                        array_col[i][j] = array_col[i + 1][j];
-                        array_col[i + 1][j] = temp;
+                    if (matrix_col[i][j] < matrix_col[i + 1][j]) {
+                        temp = matrix_col[i][j];
+                        matrix_col[i][j] = matrix_col[i + 1][j];
+                        matrix_col[i + 1][j] = temp;
                     }
                 }
             }
         }
-        int max = array_row[0][0], min = array_row[0][0];
-
         for (int i = 0; i < num_1; i++) {
             for (int k = 0; k < num_2; k++) {
-                sum += array_row[i][k];
-                if (array_row[i][k] > max) {
-                    max = array_row[i][k];
-                }
-                if (array_row[i][k] < min) {
-                    min = array_row[i][k];
-                }
+                out_col << matrix_col[i][k] << " ";
             }
-        }
-        mean = sum / (num_1 * num_2);
-        out_row << "mean = " << mean << endl;
-        out_col << "mean = " << mean << endl;
-        out_row << "max = " << max << endl;
-        out_col << "max = " << max << endl;
-        out_row << "min = " << min << endl;
-        out_col << "min = " << min << endl;
-
-        for (int i = 0; i < num_1; i++) {
-            for (int k = 0; k < num_2; k++) {
-                out_row << array_row[i][k] << " ";
-                out_col << array_col[i][k] << " ";
-            }
-            out_row << endl;
             out_col << endl;
         }
         cout << "Completed!" << endl;
-        for (int i = 0; i < num_1; i++) {
-            delete[] array_row[i], array_col[i];
-        }
-        delete[] array_row, array_col;
     }
-    
+
     in.close();
     out_row.close();
     out_col.close();
