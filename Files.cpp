b2a046e0a66b87d3cb2445c58f2d50436d392b7d
Avokazzar <65611836+Avokazzar@users.noreply.github.com>
2020-06-18 21:24:12 +0300

    Update Files.cpp

--- Files.cpp
+++ Files.cpp
@@ -2,77 +2,74 @@
 #include <fstream>
 #include <algorithm> 
 #include <vector>
-#include <string>
 #include <stdlib.h>
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
-using std::string;
 using std::min_element;
 
+bool comp1(double a, double b) {
+    return a > b;
+}
 int main()
 {
-    int num_1, num_2, temp;
+    int num_1, num_2;
     double mean, sum = 0;
 
-    std::string line;
-    std::ifstream in("D:\\input.txt");
-    std::ofstream out_row("D:\\out_row.txt", std::ios::out);
-    std::ofstream out_col("D:\\out_col.txt", std::ios::out);
+    std::ifstream in("input.txt");
+    std::ofstream out_row("out_row.txt", std::ios::out);
+    std::ofstream out_col("out_col.txt", std::ios::out);
 
     if (in.is_open() && out_row.is_open())
     {
         in >> num_1 >> num_2;
 
-        std::vector<std::vector<int>> matrix_row(num_1, std::vector<int>(num_2));
-        std::vector<std::vector<int>> matrix_col(num_1, std::vector<int>(num_2));
-        std::vector<int> minmax_str;
-        for (int i = 0; i < num_1; i++) {
-            for (int k = 0; k < num_2; k++) {
-                if (k == num_2 - 1) {
-                    getline(in, line, '\n');
-                }
-                else {
-                    getline(in, line, ',');
-                }
-                matrix_row[i][k] = stoi(line);
-                matrix_col[i][k] = stoi(line);
-                sum += matrix_row[i][k];
-                minmax_str.push_back(matrix_row[i][k]);
-            }
+        std::vector<double> matrix(num_1 * num_2);
+        for (int i = 0; i < matrix.size(); i++) {
+                in >> matrix[i];
+                in.ignore(1, ',');
+                sum += matrix[i];
         }
-        auto minmax = std::minmax_element(minmax_str.begin(), minmax_str.end());
-        mean = sum / (num_1 * num_2);
+        std::vector<double> matrix_buff(num_1 * num_2);
         
+        auto minmax = std::minmax_element(matrix.begin(), matrix.end());
+        mean = sum / (matrix.size());
+
         out_row << "mean = " << mean << endl;
         out_col << "mean = " << mean << endl;
         out_row << "max = " << *minmax.second << endl;
         out_col << "max = " << *minmax.second << endl;
         out_row << "min = " << *minmax.first << endl;
         out_col << "min = " << *minmax.first << endl;
- 
-        for (int i = 0; i < num_1; i++) {
-            sort(matrix_row[i].begin(), matrix_row[i].end());
-            for (int k = 0; k < num_2; k++) {
-                out_row << matrix_row[i][k] << " ";
+
+        for (int i = 0; i < num_1; ++i) {
+            for (int j = 0; j < num_2; ++j) {
+                matrix_buff[j * num_1 + i] = matrix[i * num_2 + j];
+            }
+        }
+        
+        for (int i = 0; i < num_1; ++i) {
+            sort(matrix.begin() + num_2 * i, matrix.begin() + num_2 * i + num_2);
+        }
+        for (int i = 0; i < num_1; ++i) {
+            for (int j = 0; j < num_2; ++j) {
+                out_row << matrix[i * num_2 + j] << " ";
             }
             out_row << endl;
         }
-        for (int j = 0; j < num_2; j++) {
-            for (int k = 0; k < num_1 - 1; k++) {
-                for (int i = 0; i < num_1 - k - 1; i++) {
-                    if (matrix_col[i][j] < matrix_col[i + 1][j]) {
-                        temp = matrix_col[i][j];
-                        matrix_col[i][j] = matrix_col[i + 1][j];
-                        matrix_col[i + 1][j] = temp;
-                    }
-                }
+        //---------------------------------------
+        for (int i = 0; i < num_2; ++i) {
+            sort(matrix_buff.begin() + num_1 * i, matrix_buff.begin() + num_1 * i + num_1, comp1);
+        }
+        for (int i = 0; i < num_1; ++i) {
+            for (int j = 0; j < num_2; ++j) {
+                matrix[i * num_2 + j] = matrix_buff[j * num_1 + i];
             }
         }
-        for (int i = 0; i < num_1; i++) {
-            for (int k = 0; k < num_2; k++) {
-                out_col << matrix_col[i][k] << " ";
+        for (int i = 0; i < num_1; ++i) {
+            for (int j = 0; j < num_2; ++j) {
+                out_col << matrix[i * num_2 + j] << " ";
             }
             out_col << endl;
         }
